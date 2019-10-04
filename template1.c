40b35ff95d128713d996679c922d431e9dad6543
codythegreat <maxiecody@gmail.com>
2019-10-03 20:30:51 -0500

    added template c file

---
+++ template1.c
@@ -0,0 +1,41 @@
+/* 
+ * $
+ */
+
+// $
+
+
+// include statement (imports)
+#include <$>
+// global definition
+#define $ "$"
+// integer
+int $ = $;
+// single character
+char $ = '$';
+// long (64bit) int
+long int $ = $;
+// float
+float $ = $;
+// float(64bit)
+double $ = $;
+// print statement
+printf("$");
+
+
+
+
+
+
+// function (main)
+main(){
+    $
+}
+// character array
+char $[] = "Cody Maxie";
+// change the contents of character array
+strcpy($, "$");
+// scan input for characters/names/numbers
+scanf(" %$", &$)
+// scan multiple input and assign to multiple variables
+scanf(" %$ %$", &$, &$)
